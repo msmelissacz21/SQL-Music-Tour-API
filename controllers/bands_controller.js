ee5218e3ae7654770996c0b966df6dc6a0dd6ea1
Melissa Zuengler <melissazuengler@Melissas-MacBook-Pro.local>
2022-07-19 19:46:06 -0500

    Created basic RESTful routes

--- controllers/bands_controller.js
+++ controllers/bands_controller.js
@@ -1,4 +1,5 @@
 // DEPENDENCIES
+const { Op } = require('sequelize')
 const bands = require('express').Router()
 const db = require('../models')
 const { Band } = db 
@@ -6,7 +7,12 @@ const { Band } = db
 // FIND ALL BANDS
 bands.get('/', async (req, res) => {
     try {
-        const foundBands = await Band.findAll()
+        const foundBands = await Band.findAll({
+            order: [ [ 'available_start_time', 'ASC'] ],
+            where: {
+                name: { [Op.like]: `%${req.query.name ? req.query.name : ''}%` }
+            }
+        })
         res.status(200).json(foundBands)
     } catch (error) {
         res.status(500).json(error)
@@ -69,8 +75,6 @@ bands.delete('/:id', async (req, res) => {
         res.status(500).json(err)
     }
 })
-
-
 
 
 // EXPORT
